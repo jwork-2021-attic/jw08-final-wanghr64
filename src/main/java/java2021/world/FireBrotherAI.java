95b9daaa8f476592081a67b21aca26058ebaae38
wanghr <239955612@qq.com>
2021-12-21 15:57:14 +0800

    fix direction bug

--- src/main/java/java2021/world/FireBrotherAI.java
+++ src/main/java/java2021/world/FireBrotherAI.java
@@ -5,21 +5,21 @@ import java.util.*;
 
 public class FireBrotherAI extends PlayerAI {
 
-    int direction;
-
     public FireBrotherAI(Creature creature, World world, List<String> messages) {
         super(creature, world, messages);
     }
 
     @Override
     public void run() {
+        System.out.println("burst fire");
         onSkill = true;
         freeze = true;
         Creature enemy = null;
         int x = player.x();
         int y = player.y();
+        System.out.println(player.preDirect);
         for (int i = 1; i < 6; ++i) {
-            switch (direction) {
+            switch (player.preDirect) {
                 case KeyEvent.VK_RIGHT:
                     enemy = world.creature(x + i, y);
                     break;
@@ -33,36 +33,13 @@ public class FireBrotherAI extends PlayerAI {
                     enemy = world.creature(x, y + i);
                     break;
             }
-            if (enemy != null)
-                player.attack(enemy);
+            player.attack(enemy);
         }
         try {
-            Thread.sleep(1000);
+            Thread.sleep(800);
         } catch (Exception e) {
         }
         freeze = false;
         onSkill = false;
-    }
-
-    @Override
-    public void onEnter(int x, int y, Tile tile) {
-        // decide which direction to burst fire.
-        int preX = player.x();
-        int preY = player.y();
-        if (x - preX == 1)
-            direction = KeyEvent.VK_RIGHT;
-        else if (x - preX == -1)
-            direction = KeyEvent.VK_LEFT;
-        else if (y - preY == 1)
-            direction = KeyEvent.VK_DOWN;
-        else if (y - preY == -1)
-            direction = KeyEvent.VK_UP;
-        if (tile.isGround()) {
-            creature.setX(x);
-            creature.setY(y);
-        } else if (tile.isDiggable() && player.digCount > 0) {
-            creature.dig(x, y);
-            --player.digCount;
-        }
     }
 }
