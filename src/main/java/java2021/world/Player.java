e3d25ed0c93601b7d7b27813c3bc1a7fbb6d2e4d
wanghr <239955612@qq.com>
2021-12-21 15:56:44 +0800

    add PlayerAIs related

--- src/main/java/java2021/world/Player.java
+++ src/main/java/java2021/world/Player.java
@@ -1,17 +1,30 @@
 package java2021.world;
 
 import java.awt.Color;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Player extends Creature {
     public Player(World world, char glyph, Color color, int maxHP, int attack, int defense, int visionRadius) {
         super(world, glyph, color, maxHP, attack, defense, visionRadius);
         this.validAIs = new boolean[7];
-        Arrays.fill(this.validAIs, false);
+        Arrays.fill(this.validAIs, true);
         this.validAIs[0] = true;
         maxCoolTime = new int[] { 0, 10, 10, 10, 10, 10, 10 };
         costCoolTime = new int[] { 0, 1, 10, 3, 7, 10, 10 };
         curCoolTime = new int[7];
+
+        messages = new ArrayList<>();
+
+        myAIs = new PlayerAI[7];
+        myAIs[0] = new OldManAI(this, world, messages);
+        myAIs[1] = new PowerBrotherAI(this, world, messages);
+        myAIs[2] = new ViewBrotherAI(this, world, messages);
+        myAIs[3] = new FireBrotherAI(this, world, messages);
+        myAIs[4] = new WaterBrotherAI(this, world, messages);
+        myAIs[5] = new SteelBrotherAI(this, world, messages);
+        myAIs[6] = new HideBrotherAI(this, world, messages);
     }
 
     public int digCount = 0;
@@ -21,17 +34,24 @@ public class Player extends Creature {
     public boolean isVisible = true;
 
     public boolean[] validAIs;
+    public int iCurAI;
+    private PlayerAI[] myAIs;
+    private List<String> messages;
 
     public int[] curCoolTime;
     public final int[] maxCoolTime;
     public final int[] costCoolTime;
 
+    public PlayerAI curAI() {
+        return myAIs[iCurAI];
+    }
+
     public boolean onSkill() {
-        return ai.onSkill();
+        return curAI().onSkill();
     }
 
     public boolean freeze() {
-        return ai.freeze();
+        return curAI().freeze();
     }
 
     public void setDefense(int d) {
@@ -60,6 +80,6 @@ public class Player extends Creature {
     }
 
     public void skill() {
-        new Thread(ai).start();
+        new Thread(curAI()).start();
     }
 }
